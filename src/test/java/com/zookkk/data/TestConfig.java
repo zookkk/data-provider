bf7633602d552632ff2cbd63aaadd9d91f8efca0
zookkk <zookkk@zookkkdeMacBook-Pro.local>
2023-04-15 14:47:08 +0800

    config .gitignore again

---
+++ src/test/java/com/zookkk/data/TestConfig.java
@@ -0,0 +1,12 @@
+package com.zookkk.data;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author zookkk <1349054970@qq.com>
+ */
+@SpringBootConfiguration
+@ComponentScan("com.zookkk")
+public class TestConfig {
+}
